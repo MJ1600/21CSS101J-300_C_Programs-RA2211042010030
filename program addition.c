d548e65624612c0e91abecae88d3058752ac0362
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-11-20 18:21:27 +0530

    Create program addition.c

---
+++ program addition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Programm ADDITION */
+ /* Written by MIMANSHA */
+ main() 
+ { 
+ int number; 
+ float amount;
+ 
+ number = 100; 
+ 
+ amount = 30.75 + 75.35;
+ printf(“%d\n”,number);
+ printf(“%5.2f”,amount); 
+ } 
