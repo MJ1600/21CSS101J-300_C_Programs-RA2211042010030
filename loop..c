e65c99341a5c16fabf281a3dea3c86d1f0e8ed6b
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-11-21 22:04:21 +0530

    Create loop..c

---
+++ loop..c
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+
+int main( )
+
+{
+
+int a, b, c, sum;
+
+printf(“\n Enter the Three Numbers: ”);  scanf(“%d%d%d”, &a,&b,&c);
+
+sum = a+b+c;
+
+printf(“The sum of 3 Numbers is %d”, sum);  return 0;
+
+}
