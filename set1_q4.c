6bdebb9d586c5936eaa78e46ec9a8a3a1c49b21f
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-11-21 19:32:15 +0530

    Create set1_q4.c

---
+++ set1_q4.c
@@ -0,0 +1,22 @@
+int main()
+{
+    int N, X, Y;
+    
+    printf("Number of problems = ");
+    scanf("%d", &N);
+    
+    printf("The Maximum score for each question = ");
+    scanf("%d", &X);
+    
+    printf("The score Amala wants = ");
+    scanf("%d", &Y);
+    
+    if((N*X) <= Y)
+    {
+        printf("Yes\n");
+    }
+    else
+    { 
+        printf("No\n");
+    }
+}
