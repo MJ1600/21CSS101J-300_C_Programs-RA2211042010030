9ca26385cf9e01a62bf8be566708cbdd6345fe61
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-11-21 23:19:01 +0530

    Create loop2.c

---
+++ loop2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+
+{
+
+	int row, j;
+
+	for(row=’A’;row<=’D’;row++)
+
+	{
+
+		for(j=’A’;j<=row;j++)
+
+		{
+
+			printf(“%c”, j);
+
+		}
+
+		printf(“\n);
+
+	}
+
+	return 0;
+
+}
