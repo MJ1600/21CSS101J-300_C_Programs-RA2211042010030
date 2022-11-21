d195e95051e464f86249ee44c38b56592408e224
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-11-21 23:50:33 +0530

    Create array3.c

---
+++ array3.c
@@ -0,0 +1,31 @@
+#include<stdio.h> 
+int main( )
+
+{
+
+int i, n, arr[5];
+
+scanf(“%d”, &n);
+
+printf(“Enter the Elements of Array\n”);
+
+for(i=0; i<n; i++)
+
+{
+
+scanf(“%d”, &a[i]);
+
+}
+
+printf(“The Elements of the Array are”\n”);
+for(i=0; i<n; i++)
+
+{
+
+printf(“%d”, a[i]);
+
+}
+
+return 0;
+
+}
