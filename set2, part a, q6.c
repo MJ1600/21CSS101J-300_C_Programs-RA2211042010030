42efa6863b70e9440d8ae63640e180ac452b5ccc
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-11-21 11:45:35 +0530

    Create set2, part a, q6.c

---
+++ set2, part a, q6.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+int units, total_bill_amount, bill_amount1, bill_amount2, bill_amount3;
+
+printf("Units of electricity used : ");
+scanf("%lf", &units);
+
+if(units =< 50) {
+bill_amount1 = (units * 0.50) }
+
+else if(50 < units < 150) {
+bill_amount2 = (units * 0.75) }
+
+else if(150 < units 250) {
+bill_amount3 = (units * 1.5) }
+
+total_bill_amount = bill_amount1 + bill_amount2 + bill_amount3;
+
+if(total_bill_amount > 300)
+{
+  printf("Total Bill Amount = 
+         
+         
+         
+         
+         
+         
+         
