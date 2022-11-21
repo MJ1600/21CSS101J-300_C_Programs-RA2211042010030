f52a3314931069444b25d43e53d39aa0adc254aa
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-11-21 23:11:22 +0530

    Create set1_q7.c

---
+++ set1_q7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int i, j, n;
+
+printf("Enter the number of rows:");
+scanf("%d", &i);
+
+for(i=0, i<n, i++)
+{
+printf("\n");
+for(j=0, j<n, j--)}
+
+printf("%d", &n);
+return 0;
+}
