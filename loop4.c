74af518b8401cf672b532ac05080ed8e2e06869c
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-11-21 23:22:46 +0530

    Create loop4.c

---
+++ loop4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+	int row, j;
+
+	for(row=1;row<=5;row++)
+
+	{
+
+		for(j=1;j<=5;j++)
+
+		{
+
+			printf(“%d”,j);
+
+		}
+
+		printf(“\n”);
+
+	}
+
+	return 0;
+
+}
+
