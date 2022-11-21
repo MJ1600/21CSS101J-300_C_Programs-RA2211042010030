8ece78f037e80bfcd214a07b134b3273f75e06c9
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-11-21 23:26:12 +0530

    Create array..c

---
+++ array..c
@@ -0,0 +1,28 @@
+#include<stdio.h>  
+int main( )
+
+{
+
+int i, arr[5];
+
+arr[0] = 10;
+
+arr[1] = 20;
+
+arr[2] = 30;
+
+arr[3] = 40;
+
+arr[4] = 50;
+
+for(i=0; i<=n; i++)
+
+{
+
+printf(“%d”\n, a[i]);
+
+}
+
+return 0;
+
+}
