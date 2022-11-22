9e2fae5d35e6e43e41c4e841734cc4774d6435d8
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-11-22 11:50:17 +0530

    Create strrev.c

---
+++ strrev.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+   char str[50] = "Live";
+   
+   printf("The given string is = %s\n",str);
+   printf("After reversing sting is = %s\n",strrev(str));
+   
+   return 0;
+}
