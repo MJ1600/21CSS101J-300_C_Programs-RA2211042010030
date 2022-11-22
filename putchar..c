19a6bdcb4f5bc7973272f9d980b4feebceeebd02
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-11-22 10:04:28 +0530

    Create putchar..c

---
+++ putchar..c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ #include <ctype.h>
+ main()
+ {
+ char alphabet;
+ printf(“Enter an alphabet”);
+ putchar(‘\n’); 
+   /* move to next line */
+ alphabet = getchar();
+ if (islower(alphabet))
+ putchar(toupper(alphabet));
+   /* Reverse and display */
+ else
+ putchar(tolower(alphabet)); 
+   /* Reverse and display */
+ }
