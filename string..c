a84044c8ba56ecda0bb7e17f9f5d1076c1d49b70
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-11-22 10:20:04 +0530

    Create string..c

---
+++ string..c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+ {
+ int m = 12345;
+ long n = 987654;
+ printf(“%d\n”,m);
+ printf(“%10d\n”,m);
+ printf(“%010d\n”,m);
+ printf(“%-10d\n”,m);
+ printf(“%10ld\n”,n);
+ printf(“%10ld\n”,-n);
+ }
