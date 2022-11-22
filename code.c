45982cce90c4908840d0f237551dce435cc0b3d2
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-11-22 09:46:57 +0530

    Create code.c

---
+++ code.c
@@ -0,0 +1,10 @@
+#include <Stdio.h>
+int main ()
+ {
+ int months, days ;
+ printf(“Enter days\n”) ;
+ scanf(“%d”, &days) ;
+ months = days / 30 ;
+ days = days % 30 ;
+ printf(“Months = %d Days = %d”, months, days) ;
+ }
