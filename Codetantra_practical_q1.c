710e326425685567014b7848208a3e99c11f2aab
Mimansha (RA22110420110030) <113929710+MJ1600@users.noreply.github.com>
2022-11-23 21:28:11 +0530

    Create Codetantra_practical_q1.c

---
+++ Codetantra_practical_q1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+
+int a = 20, b = 3;
+
+printf("Addition Result = %d\n", (a+b));
+
+printf("Subtraction Result = %d\n", (a-b));
+
+printf("Multiplication Result = %d\n", (a*b));
+
+printf("Division Result = %d\n", (a/b));
+
+printf("Reneinder = %d\n", (a%b)); 
+return 0;
+}
